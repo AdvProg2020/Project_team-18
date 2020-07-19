029f27eabbcaaaf80a02760031ca4d6f2394f543
dorsa-m <dorsa.majdi@gmail.com>
2020-07-19 15:23:13 +0430

    token

---
+++ src/main/java/Server/Token.java
@@ -0,0 +1,11 @@
+package Server;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Token {
+    private ArrayList<Object> iss;
+    private boolean exp;
+    private LocalDateTime creationTime;
+
+}
