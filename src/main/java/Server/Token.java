e00c9afb9effbea71aef76ea79787dcfc1dd1dd3
dorsa-m <dorsa.majdi@gmail.com>
2020-07-20 00:45:51 +0430

    token

--- src/main/java/Server/Token.java
+++ src/main/java/Server/Token.java
@@ -1,11 +1,39 @@
 package Server;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.TextCodec;
+
+import javax.xml.bind.DatatypeConverter;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
+
+import static javax.crypto.Cipher.SECRET_KEY;
 
 public class Token {
-    private ArrayList<Object> iss;
-    private boolean exp;
-    private LocalDateTime creationTime;
+    private static SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+    public static String createJWS(long validPeriod){
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+            long expMillis = nowMillis + validPeriod;
+           Date exp = new Date(expMillis);
 
+        String jws = Jwts.builder()
+                .setSubject("authenticationToken")
+                .setId("id")
+                .setExpiration(exp)
+                .signWith(
+                        signatureAlgorithm,
+                        TextCodec.BASE64.decode("Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=")
+                )
+                .compact();
+        return jws;
+    }
+    public static void readJWS(String jws){
+        Jwts.parser()
+                .setSigningKey(TextCodec.BASE64.decode("Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E="))
+                .parseClaimsJws(jws).getBody();
+    }
 }
